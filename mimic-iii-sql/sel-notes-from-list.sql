e06daa4fce1b3151ea38a55fbdbeabfb490d03ca
Nasser Ghadiri <4505706+nghadiri@users.noreply.github.com>
2025-09-05 16:36:40 +1000

    Update links

--- mimic-iii-sql/sel-notes-from-list.sql
+++
@@ -1,6 +0,0 @@
-select hadm_id,chartdate,charttime,text
-from mimiciii.noteevents mn
---where hadm_id in (192314,156372,127789)
---	where hadm_id in (129767,196486,103541)
-	where hadm_id in (139289,169684,186273)
-order by text,hadm_id,chartdate,charttime
